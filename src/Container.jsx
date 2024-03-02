4dee231853d7fe3a4132418ac879bd262baac12c
Elvio14 <andrew.elvio14@gmail.com>
2024-03-01 19:25:37 -0500

    default white

--- src/Container.jsx
+++ src/Container.jsx
@@ -5,21 +5,6 @@ import Panel from './Panel.jsx'
 import './main.css'
 import { useState } from 'react'
 
-const averageHexColor = (c1, c2) => {
-    const hexToRgb = hex =>
-      hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
-                 ,(m, r, g, b) => '#' + r + r + g + g + b + b)
-          .substring(1).match(/.{2}/g)
-          .map(x => parseInt(x, 16));
-  
-    const rgb1 = hexToRgb(c1);
-    const rgb2 = hexToRgb(c2);
-  
-    const avgRgb = rgb1.map((comp, i) => (comp + rgb2[i]) / 2);
-  
-    return "#" + avgRgb.map(x => Math.round(x).toString(16).padStart(2, '0')).join('');
-}
-
 const Container = () => {
     const [currentColor, setCurrentColor] = useState()
     const hanldePresetChange = (value) => {
